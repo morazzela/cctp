5a78ce7d9f1871e1cf6aba6f1b9aa1464579b2dd
morazzela <morazzela@protonmail.com>
2025-05-15 20:42:34 +0200

    replace usdc addresses

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -19,7 +19,7 @@ export const CHAINS_CONFIG: { [key: number]: ChainsConfigType } = {
     tokenMessenger: "0x28b5a0e9C621a5BadaA536219b3a228C8168cf5d",
     messageTransmitter: "0x81D40F21F12A8F0E3252Bccb954D722d4c464B64",
     tokenMinter: "0xfd78EE919681417d192449715b2594ab58f5D002",
-    usdc: "0x29219dd400f2Bf60E5a23d13Be72B486D4038894",
+    usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     iconUri:
       "https://tokens.debridge.finance/Logo/1/0x0000000000000000000000000000000000000000/small/token-logo.svg",
     fastAvailable: true,
@@ -43,7 +43,7 @@ export const CHAINS_CONFIG: { [key: number]: ChainsConfigType } = {
     tokenMessenger: "0x28b5a0e9C621a5BadaA536219b3a228C8168cf5d",
     messageTransmitter: "0x81D40F21F12A8F0E3252Bccb954D722d4c464B64",
     tokenMinter: "0xfd78EE919681417d192449715b2594ab58f5D002",
-    usdc: "0x29219dd400f2Bf60E5a23d13Be72B486D4038894",
+    usdc: "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
     iconUri:
       "https://tokens.debridge.finance/Logo/43114/0x0000000000000000000000000000000000000000/small/token-logo.svg",
     fastAvailable: false,
@@ -55,7 +55,7 @@ export const CHAINS_CONFIG: { [key: number]: ChainsConfigType } = {
     tokenMessenger: "0x28b5a0e9C621a5BadaA536219b3a228C8168cf5d",
     messageTransmitter: "0x81D40F21F12A8F0E3252Bccb954D722d4c464B64",
     tokenMinter: "0xfd78EE919681417d192449715b2594ab58f5D002",
-    usdc: "0x29219dd400f2Bf60E5a23d13Be72B486D4038894",
+    usdc: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
     iconUri:
       "https://tokens.debridge.finance/Logo/8453/0x0000000000000000000000000000000000000000/small/token-logo.svg",
     fastAvailable: true,
@@ -67,7 +67,7 @@ export const CHAINS_CONFIG: { [key: number]: ChainsConfigType } = {
     tokenMessenger: "0x28b5a0e9C621a5BadaA536219b3a228C8168cf5d",
     messageTransmitter: "0x81D40F21F12A8F0E3252Bccb954D722d4c464B64",
     tokenMinter: "0xfd78EE919681417d192449715b2594ab58f5D002",
-    usdc: "0x29219dd400f2Bf60E5a23d13Be72B486D4038894",
+    usdc: "0x176211869cA2b568f2A7D4EE941E073a821EE1ff",
     iconUri:
       "https://tokens.debridge.finance/Logo/59144/0x0000000000000000000000000000000000000000/small/token-logo.svg",
     fastAvailable: true,
@@ -79,7 +79,7 @@ export const CHAINS_CONFIG: { [key: number]: ChainsConfigType } = {
     tokenMessenger: "0x28b5a0e9C621a5BadaA536219b3a228C8168cf5d",
     messageTransmitter: "0x81D40F21F12A8F0E3252Bccb954D722d4c464B64",
     tokenMinter: "0xfd78EE919681417d192449715b2594ab58f5D002",
-    usdc: "0x29219dd400f2Bf60E5a23d13Be72B486D4038894",
+    usdc: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
     iconUri:
       "https://tokens.debridge.finance/Logo/42161/0x0000000000000000000000000000000000000000/small/token-logo.svg",
     fastAvailable: true,
