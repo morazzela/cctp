e8034868b37ccdc5f5c6ece15eb31b1682e93524
morazzela <morazzela@protonmail.com>
2025-09-16 22:18:42 +0200

    fix

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -210,7 +210,7 @@ export const HYPEREVM = createChainFromNetwork(hyperevm, {
   tokenMessengerV2: "0x28b5a0e9C621a5BadaA536219b3a228C8168cf5d",
   messageTransmitterV2: "0x81D40F21F12A8F0E3252Bccb954D722d4c464B64",
   tokenMinterV2: "0xfd78EE919681417d192449715b2594ab58f5D002",
-  usdc: "0x79A02482A880bCE3F13e09Da970dC34db4CD24d1",
+  usdc: "0xb88339CB7199b77E23DB6E890353E22632Ba630f",
   hasV1: false,
   hasV2: true,
 });
